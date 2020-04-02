5093054353b38781608ac71b55b361ad28a1cb85
pallavi-jain <pallavi.jain2610@gmail.com>
2020-04-02 22:38:28 +0530

    angular assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddressComponent } from './app.address.component';
+import { UserComponent } from './app.user.component';
+import { CompanyComponent} from './app.company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,AddressComponent, UserComponent, CompanyComponent
   ],
   imports: [
     BrowserModule
